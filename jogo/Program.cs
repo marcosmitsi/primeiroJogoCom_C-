ced79ced164092dc7496f26f484aecba50b34d5d
Marcos Mitsi <marcos.mitsi@hotmail.com>
2024-01-04 20:57:58 -0300

    commit inicial

---
+++ jogo/Program.cs
@@ -0,0 +1,23 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do
+{
+    Console.WriteLine("Digite um número entre 1 e 100");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número é maior");
+    }
+    else 
+    {
+        Console.WriteLine("O número é menor");
+    }
+
+} while (true);
